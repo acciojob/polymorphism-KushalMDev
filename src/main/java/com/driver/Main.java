43ced9dce6e46b98fb752c9fc0013f657ded959c
KushalMDev <kushm1929@gmail.com>
2023-12-13 19:34:50 +0530

    My first assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Product p=new Product();
+        int ans1=p.product(23, 25);
+        int ans2=p.product(2, 3, 4);
+        double d=p.product(10.0, 20.0);
+    }
+    public static class Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
 }
